11ab4f50328baaad4b0cccf0fa90d3f4cc314e8f
Aditi Kadam <iamaditikadam@gmail.com>
2023-11-01 23:16:21 +0530

    Added the button

--- practice/src/App.js
+++ practice/src/App.js
@@ -48,11 +48,31 @@ export default function MyButton(){
   )
 }*/
 
-import {useState} from 'react';
+import { useState } from 'react';
 
-export default function App()
-{]]}
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Counters that update separately</h1>
+      <MyButton />
+      <MyButton />
+    </div>
+  );
+}
 
+function MyButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Clicked {count} times
+    </button>
+  );
+}
 
 
 
