df80b79ffb1331d830893d7d3b07ffc87a3c0442
Aditi Kadam <iamaditikadam@gmail.com>
2023-11-02 19:40:37 +0530

    Footer done

--- practice/src/App.js
+++ practice/src/App.js
@@ -76,11 +76,14 @@ function MyButton() {
 
 import "./App.css";
 import Header from "./Header";
+import Footer from "./Footer";
+
 
 export default function App(){
   return(
     <div className="App">
       <Header />
+      <Footer />
     </div>
   )
 }
