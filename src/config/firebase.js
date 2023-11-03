e2ad65f8f42ed3afd02d622ec068bed7262511b8
Aditi Kadam <iamaditikadam@gmail.com>
2023-11-03 12:53:33 +0530

    imported firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBv0RAg_KjsMWLoySjlb9OHpRj4-9X064g",
+    authDomain: "trial-7ef0b.firebaseapp.com",
+    projectId: "trial-7ef0b",
+    storageBucket: "trial-7ef0b.appspot.com",
+    messagingSenderId: "834744248289",
+    appId: "1:834744248289:web:50dbd7e4998dbc24e414a0",
+    measurementId: "G-VS7JY865PQ"
+  };
+
+  // Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export default firebase;
